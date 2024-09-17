6de43115c7ffc5d67ebd168bd3dddc058b39ad4f
Waihon Yew <yewwaihon@gmail.com>
2024-09-17 18:22:48 +0800

    Explore features of Spring Data JPA

--- src/main/java/com/waihon/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/waihon/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -17,10 +17,21 @@ public class CourseCommandLineRunner implements CommandLineRunner {
         repository.save(new Course(1, "Learn AWS Spring Data JPA Now!", "in28minutes"));
         repository.save(new Course(2, "Learn Azure Spring Data JPA Now!", "in28minutes"));
         repository.save(new Course(3, "Learn DevOps Spring Data JPA Now!", "in28minutes"));
+        repository.save(new Course(4, "Full Stack React and Java Spring Boot", "luv2code"));
 
         repository.deleteById(2l);
 
         System.out.println(repository.findById(1l));
         System.out.println(repository.findById(3l));
+
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
+
+        System.out.println(repository.findByAuthor("in28minutes"));
+        System.out.println(repository.findByAuthor(""));
+        System.out.println(repository.findByAuthor("luv2code"));
+
+        System.out.println(repository.findByName("Learn DevOps Spring Data JPA Now!"));
+        System.out.println(repository.findByName("Full Stack"));
     }
 }
--- src/main/java/com/waihon/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
+++ src/main/java/com/waihon/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -3,5 +3,11 @@ package com.waihon.learnjpaandhibernate.course.springdatajpa;
 import com.waihon.learnjpaandhibernate.course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+
+    public List<Course> findByName(String name);
+
+    public List<Course> findByAuthor(String author);
 }
